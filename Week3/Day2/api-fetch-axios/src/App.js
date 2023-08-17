1b47c2bc955501446b01069d72530966d5fca12d
Wael Hajji <waelhajji.contact@gmail.com>
2023-08-17 11:05:44 +0100

    Week3Day2

--- Week3/Day2/api-fetch-axios/src/App.js
+++ Week3/Day2/api-fetch-axios/src/App.js
@@ -1,109 +1,120 @@
-
-import { useState, useEffect } from 'react';
-
-import axios from "axios"
+import Axios from "axios"
 import './App.css';
+import { useEffect, useState } from 'react';
+// import GitHubApi from "./components/GitHubApi";
 
 function App() {
 
 
+  // anything dynamic needs STATE
   const [heroes, setHeroes] = useState([])
+  //!-------------------------UseEffect
+  console.log("1.before useEffect")
+  useEffect(() => {
+    Axios.get("https://akabab.github.io/superhero-api/api/all.json")
+      .then(res => {
+        // Axios Wraps everything in .data
+        console.log(res.data)
+        setHeroes(res.data)
+      }).catch(err => {
+        console.log(err)
+      })
+
+  }, [])
+  console.log("3.after useEffect")
+
+
+  //!-------------------------Axios
+  const AxiosHeroes = () => {
+    Axios.get("https://akabab.github.io/superhero-api/api/all.json")
+      .then(res => {
+        // Axios Wraps everything in .data
+        console.log(res.data)
+        setHeroes(res.data)
+      }).catch(err => {
+        console.log(err)
+      })
+  }
 
 
   //!-------------------------fetch
   const fetchHeroes = () => {
-    console.log("clicked")
     fetch("https://akabab.github.io/superhero-api/api/all.json")
       .then(response => {
         return response.json()
-      })
-      .then(response => {
+      }).then(response => {
+        console.log(response)
         setHeroes(response)
       })
       .catch(err => {
         console.log(err);
       })
+
   }
-
-
-  //!-------------------------Axios
-  const axiosHeroes = () => {
-    axios.get("https://akabab.github.io/superhero-api/api/all.json")
-      .then(response => {
-        console.log("Axios ===>", response)
-        setHeroes(response.data)
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-  //!-------------------------useEffect
-
-  useEffect(() => {
-    axios.get("https://akabab.github.io/superhero-api/api/all.json")
-      .then(response => {
-        console.log("Axios ===>", response)
-        setHeroes(response.data)
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-  }, [])
-
-
 
 
 
 
   return (
-    <fieldset className="App">
-      <legend>App.jsx</legend>
+    <div className="App">
       <h1> 🦸 🦹  SupperHeroes 🦸‍♀️ 🦹‍♀️ </h1>
       <h4>fetch /Axios / useEffect</h4>
       <hr />
-
-
-      <button onClick={fetchHeroes}>Fetch SupperHeroes</button> <br />
-      <button onClick={axiosHeroes}>Axios  SupperHeroes</button> <br />
-
       {/* {JSON.stringify(heroes)} */}
-      <table>
+      <button className="btn btn-success" onClick={fetchHeroes}> fetch SupperHeroes</button>
+      <button className="btn btn-primary" onClick={AxiosHeroes}> Axios SupperHeroes</button>
+      <hr />
+      <table >
         <thead>
-          <tr >
-            <th>Image</th>
-            <th>Hero Name</th>
-            <th>Full Name</th>
-            <th>Publisher</th>
+          <tr>
+            <th scope="col">
+              #
+            </th>
+            <th scope="col">
+              Image
+            </th>
+            <th scope="col">
+              Hero Name
+            </th >
+            <th scope="col">
+              Full Name
+            </th>
+            <th scope="col">
+              Publisher
+            </th>
           </tr>
         </thead>
         <tbody>
           {
-            heroes.map((hero, idx) => {
+            heroes.map(hero => {
               return (
+                <tr key={hero.id}>
+                  <th scope="row">{hero.id}</th>
+                  <td>
+                    <img src={hero.images.sm}
+                      width="120px"
+                    />
 
-                <tr key={idx}>
-                  <th>
-                    <img src={hero.images.sm} style={{ width: "100px" }} />
-                  </th>
-                  <th>
+                  </td>
+                  <td>
                     {hero.name}
-                  </th>
-                  <th>
-                    {hero.biography.fullName ? hero.biography.fullName : "Ma3douch 2esm"}
-                  </th>
-                  <th>
-                    {hero.biography.publisher ? hero.biography.publisher : "NO Data"}
-                  </th>
+                  </td>
+                  <td>
+                    {hero.biography.fullName ? hero.biography.fullName : "No Data"}
+                  </td>
+                  <td>
+                    {hero.biography.publisher}
+                  </td>
                 </tr>
+
               )
             })
           }
-
         </tbody>
-      </table>
 
-    </fieldset>
+      </table>
+      {/* <GitHubApi /> */}
+    </div>
   );
 }
 
---
+++ Week3/Day2/api-fetch-axios/src/components/GithubProfile.jsx
@@ -0,0 +1,31 @@
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const GithubProfile = () => {
+
+    // anything dynamic needs STATE
+    const [Github, setGithub] = useState([])
+
+    //!-------------------------UseEffect
+    useEffect(() => {
+        Axios.get("https://api.github.com/users/waeeelhajji")
+            .then(res => {
+                // Axios Wraps everything in .data
+                console.log(res.data)
+                setGithub(res.data)
+            }).catch(err => {
+                console.log(err)
+            })
+
+    }, [])
+    return (
+        <>
+            <h1>Github</h1>
+            <h2>{Github.name}</h2>
+            <h2>{Github.location}</h2>
+            <h2>{Github.company}</h2>
+        </>
+    )
+}
+
+export default GithubProfile
